d8a2db2f47c3e33584439b376ce1c4c4edb4d50c
Yuri Motoshima <yurimotoshima@gmail.com>
2022-02-04 09:05:37 -0300

    fix: log.py format altered and add nem variable for disable log

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import getenv, getcwd
 from dotenv import load_dotenv
 import pandas as pd
+import logging
 from utils_api_pipefy.libs.log import log
 from utils_api_pipefy.libs.engine import Engine
 
@@ -11,11 +12,11 @@ if __name__ == "__main__":
     
     eng = Engine()
     # ALGUMAS DAS UTILIDADES DO ENGINE
-    print(eng.columns)
-    print(eng.phase_id)
-    print(eng.fields)
-    print(eng.phases)
+    logging.info(eng.columns)
+    # print(eng.phase_id)
+    # print(eng.fields)
+    # print(eng.phases)
     
-    df = pd.DataFrame(data=eng.run_all_data_phases(), columns=eng.columns)
+    # df = pd.DataFrame(data=eng.run_all_data_phases(), columns=eng.columns)
     
     print("")
--- utils_api_pipefy/libs/log.py
+++ utils_api_pipefy/libs/log.py
@@ -9,33 +9,45 @@ from urllib3.connectionpool import log as urllib_log
 load_dotenv(dotenv_path=fr"{os.getcwd()}\.env")
 
 class log:
-    def __init__(self, name_file_log : str = None, dev_env : str = None) -> None:
-        self.name_file_log = os.getenv("LOGNAME") if name_file_log == None else name_file_log
+    def __init__(self, name_file_log : str = None, dev_env : str = None, disable_log : str = None) -> None:
+        self.name_file_log = os.getenv("LOGNAME") if name_file_log == None else name_file_log                                                                           # ¥
         self.dev_env = os.getenv("LOGENV") if dev_env == None else dev_env
+        self.disable_log = os.getenv("DISABLELOG")  if disable_log == None else dev_env
             
             
     def loginit(self):
-
-        filename = f'{self.name_file_log} - {datetime.now().strftime("%d-%m-%Y %H")}.log'
-        filename = f'[DEV] {filename}' if self.dev_env == 'DEV' else filename
-
-        dirname = f'{os.getcwd()}\\logs'
-        os.makedirs(dirname, exist_ok=True)
-
-        dirname = f'{os.getcwd()}\\logs\\{datetime.now().strftime("%d-%m-%Y")}'
-        os.makedirs(dirname, exist_ok=True)
         
-        full_filename = os.path.join(dirname, filename)
+        if self.disable_log == 'True':
+            
+            filename = f'{self.name_file_log} - {datetime.now().strftime("%d-%m-%Y %H")}.log'
+            filename = f'[DEV] {filename}' if self.dev_env == 'DEV' else filename
 
-        formatter = '[%(levelname)s]: [%(filename)s line - %(lineno)d] | Date_Time: %(asctime)s | Function: [%(funcName)s] | Message: \n ==> %(message)s'
+            dirname = f'{os.getcwd()}\\logs'
+            os.makedirs(dirname, exist_ok=True)
 
-        file_handler = logging.FileHandler(filename=full_filename)
-        stdout_handler = logging.StreamHandler(sys.stdout)
-        handlers = [file_handler, stdout_handler]
+            dirname = f'{os.getcwd()}\\logs\\{datetime.now().strftime("%d-%m-%Y")}'
+            os.makedirs(dirname, exist_ok=True)
+            
+            full_filename = os.path.join(dirname, filename)
 
-        logging.basicConfig(level=logging.DEBUG,
-                            format=formatter, handlers=handlers, )
+            formatter = '[%(levelname)s]: [%(filename)s line - %(lineno)d] | Date_Time: %(asctime)s | Function: [%(funcName)s] | Message: ➪ %(message)s'
 
-        # Remove selenium and urlib3 log
-        if self.dev_env == 'PROD':
-            urllib_log.disabled = True
+            file_handler = logging.FileHandler(filename=full_filename)
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            handlers = [file_handler, stdout_handler]
+
+            logging.basicConfig(level=logging.DEBUG,
+                                format=formatter, handlers=handlers, )
+
+            # Remove selenium and urlib3 log
+            if self.dev_env == 'PROD':
+                urllib_log.disabled = True
+        else:
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            handlers = [stdout_handler]
+
+            logging.basicConfig(level=logging.DEBUG, handlers=handlers)
+            
+            # Remove selenium and urlib3 log
+            if self.dev_env == 'PROD':
+                urllib_log.disabled = True
